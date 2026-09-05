d8f85d6624dc1df3e24bdb97ed00a7aaaafc7a68
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-05 22:19:51 +0100

    feat: load course mapping model

--- facodi_learning/models/__init__.py
+++ facodi_learning/models/__init__.py
@@ -1,6 +1,7 @@
 from . import analysis_job
 from . import analysis_result
 from . import learning_mapping
+from . import course_mapping
 from . import res_config_settings
 from . import slide_slide
 from . import slide_channel
