0078298ac442e0dea83491a3e1fa557c04b72222
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-05 22:46:04 +0100

    feat: export course mapping service

--- facodi_learning/services/__init__.py
+++ facodi_learning/services/__init__.py
@@ -1,5 +1,14 @@
 from .analysis import analyze_local_metadata
 from .analysis import normalize_output
+from .course_mapping import (
+    COURSE_MAPPING_RANKING_VERSION,
+    PROPOSAL_MIN_CONFIDENCE,
+    course_mapping_candidates,
+    propose_course_mappings,
+    rank_course_pair,
+    retrieve_course_candidates,
+)
+from .course_profile import COURSE_PROFILE_VERSION, build_course_profile
 from .course_selection import (
     EVALUATION_POLICY_VERSION,
     SELECTION_POLICY_VERSION,
@@ -9,4 +18,3 @@ from .course_selection import (
     get_course_selection_policy,
     normalize_course_title,
 )
-from .course_profile import COURSE_PROFILE_VERSION, build_course_profile
