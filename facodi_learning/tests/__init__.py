9dd085dd09c7bafbb7e6dacf7bfc3360a17b533a
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-07 05:08:23 +0100

    test: register curriculum-aware candidate selection tests

--- facodi_learning/tests/__init__.py
+++ facodi_learning/tests/__init__.py
@@ -14,3 +14,4 @@ from . import test_course_mapping_ui
 from . import test_curriculum_reference
 from . import test_curriculum_coverage
 from . import test_curriculum_gap
+from . import test_curriculum_candidate_selection
