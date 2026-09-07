675ad93fa0f60cfd5ee8ff018caa917c3cb54c26
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-07 04:44:24 +0100

    feat: load curriculum reference models

--- facodi_learning/models/__init__.py
+++ facodi_learning/models/__init__.py
@@ -2,6 +2,7 @@ from . import analysis_job
 from . import analysis_result
 from . import learning_mapping
 from . import course_mapping
+from . import curriculum_reference
 from . import res_config_settings
 from . import slide_slide
 from . import slide_channel
