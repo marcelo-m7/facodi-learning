fc1404de036c7a56866ca4aab579f11e101a1d1f
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-25 15:44:12 +0200

    feat(submission): export metadata discovery service

--- facodi_learning/services/__init__.py
+++ facodi_learning/services/__init__.py
@@ -1,6 +1,6 @@
 from .analysis import analyze_local_metadata
 from .analysis import normalize_output
-from .supabase_edge import analyze_supabase_edge
+from .supabase_edge import analyze_supabase_edge, discover_resource_metadata
 from .course_mapping import (
     COURSE_MAPPING_RANKING_VERSION,
     PROPOSAL_MIN_CONFIDENCE,
