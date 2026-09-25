0e79c8cd986907e54de603a12966e20a5096ddf9
Marcelo Santos <117441129+marcelo-m7@users.noreply.github.com>
2026-09-25 17:58:10 +0100

    chore(analysis): remove duplicate Supabase error import

--- facodi_learning/models/analysis_job.py
+++ facodi_learning/models/analysis_job.py
@@ -7,7 +7,6 @@ from ..services.analysis import normalize_output
 
 from ..services import analyze_local_metadata, analyze_supabase_edge
 from ..services.supabase_edge import SupabaseAnalysisError
-from ..services.supabase_edge import SupabaseAnalysisError
 
 _logger = logging.getLogger(__name__)
 
